aa942448b64b24ab421e92fec82ec10f4a8bed6f
Steve Phillips <kingcoyote85@gmail.com>
2021-09-18 20:58:35 -0700

    Renaming "state" to "location" when referring to where the card is at (deck, ussr hand, usa hand, discard, etc) to avoid confusion with the Vuex state.

--- src/store/index.js
+++ src/store/index.js
@@ -12,13 +12,13 @@ export const mutations = {
     if (data && (data.optional ?? false)) {
       defaultCards
         .filter((card) => (card.flags || []).includes("optional"))
-        .forEach((card) => card.state = "inactive")
+        .forEach((card) => card.location = "inactive")
     }
 
     // set all early inactive cards to deck
     defaultCards
-      .filter((card) => card.phase === "early" && card.state !== "disabled")
-      .forEach((card) => card.state = "deck")
+      .filter((card) => card.phase === "early" && card.location !== "disabled")
+      .forEach((card) => card.location = "deck")
 
     if (state.cards === undefined) {
       state.cards = defaultCards
@@ -29,21 +29,21 @@ export const mutations = {
     }
   },
   moveCard(state, data) {
-    data.card.state = data.destination
+    data.card.location = data.destination
   },
   reshuffle(state) {
-    state.cards.forEach((card) => card.state = card.state === "discard" ? "deck" : card.state);
+    state.cards.forEach((card) => card.location = card.location === "discard" ? "deck" : card.location);
   },
   addPhase(state, data) {
     state.cards
       .filter((card) => card.phase === data.phase)
-      .forEach((card) => card.state = "deck")
+      .forEach((card) => card.location = "deck")
   }
 }
 
 export const getters = {
-  cardsWithState: (state) => (targetState) => {
-    return state.cards.filter((card) => card.state === targetState)
+  cardsInLocation: (state) => (location) => {
+    return state.cards.filter((card) => card.location === location)
   }
 }
 
--- test/mutations.spec.js
+++ test/mutations.spec.js
@@ -8,13 +8,13 @@ describe('twilight store', () => {
         const state = { }
         newGame(state)
         expect(state.hasOwnProperty("cards")).to.equal(true);
-        expect(getters.cardsWithState(state)("deck").length).to.equal(36)
-        expect(getters.cardsWithState(state)("inactive").length).to.equal(6)
+        expect(getters.cardsInLocation(state)("deck").length).to.equal(36)
+        expect(getters.cardsInLocation(state)("inactive").length).to.equal(6)
     })
     it('creates new game with optional cards (104-110)', () => {
         const state = { }
         newGame(state, { optional: true })
-        expect(getters.cardsWithState(state)("deck").length).to.equal(39)
+        expect(getters.cardsInLocation(state)("deck").length).to.equal(39)
     })
     it('moves cards between states', () => {
         const state = { }
@@ -22,16 +22,16 @@ describe('twilight store', () => {
         expect(state.hasOwnProperty("cards")).to.equal(true);
 
         moveCard(state, { card: state.cards[0], destination: "ussr" })
-        expect(state.cards[0].state).to.equal("ussr")
+        expect(state.cards[0].location).to.equal("ussr")
 
         moveCard(state, { card: state.cards[1], destination: "usa" })
-        expect(state.cards[1].state).to.equal("usa")
+        expect(state.cards[1].location).to.equal("usa")
 
         moveCard(state, { card: state.cards[2], destination: "removed" })
-        expect(state.cards[2].state).to.equal("removed")
+        expect(state.cards[2].location).to.equal("removed")
 
         moveCard(state, { card: state.cards[3], destination: "discard" })
-        expect(state.cards[3].state).to.equal("discard")
+        expect(state.cards[3].location).to.equal("discard")
     })
     it('reshuffles discard into deck', () => {
         const state = { }
@@ -44,32 +44,32 @@ describe('twilight store', () => {
         moveCard(state, { card: state.cards[4], destination: "discard" })
 
         reshuffle(state)
-        expect(state.cards[0].state).to.equal("deck")
-        expect(state.cards[1].state).to.equal("usa")
-        expect(state.cards[2].state).to.equal("ussr")
-        expect(state.cards[3].state).to.equal("removed")
-        expect(state.cards[4].state).to.equal("deck")
+        expect(state.cards[0].location).to.equal("deck")
+        expect(state.cards[1].location).to.equal("usa")
+        expect(state.cards[2].location).to.equal("ussr")
+        expect(state.cards[3].location).to.equal("removed")
+        expect(state.cards[4].location).to.equal("deck")
     })
     it("adds mid war phase cards", () => {
         const state = { }
         newGame(state)
 
-        expect(getters.cardsWithState(state)("deck").length).to.equal(36)
+        expect(getters.cardsInLocation(state)("deck").length).to.equal(36)
 
         addPhase(state, { phase: "mid" })
-        expect(getters.cardsWithState(state)("deck").length).to.equal(39)
+        expect(getters.cardsInLocation(state)("deck").length).to.equal(39)
     })
     it("gets cards with state", () => {
         const state = { }
         newGame(state)
 
-        let cardsWithState = getters.cardsWithState(state)
-        expect(cardsWithState("deck").length).to.equal(36)
+        let cardsInLocation = getters.cardsInLocation(state)
+        expect(cardsInLocation("deck").length).to.equal(36)
 
         moveCard(state, { card: state.cards[0], destination: "ussr" })
-        expect(cardsWithState("ussr").length).to.equal(1)
-        expect(cardsWithState("deck").length).to.equal(35)
-        expect(cardsWithState("usa").length).to.equal(0)
+        expect(cardsInLocation("ussr").length).to.equal(1)
+        expect(cardsInLocation("deck").length).to.equal(35)
+        expect(cardsInLocation("usa").length).to.equal(0)
         
     })
 })
